624dec87310cbf97cf1e76d3de29493185170ef7
Haw Wei <47056525+hawwei913@users.noreply.github.com>
2025-07-11 21:03:28 +0800

    feat: update not found screen

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,22 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import ScreenView from "@/components/ScreenView";
+import { Text } from "@/components/ui/text";
+import { Link } from "expo-router";
+import { View } from "react-native";
+import { Button } from "~/components/ui/button";
 
 export default function NotFoundScreen() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+    <ScreenView>
+      <View className="flex-1 items-center justify-center gap-6 p-6">
+        <Text className="text-xl font-bold text-center">
+          This screen doesn't exist.
+        </Text>
+        <Link href="/" asChild>
+          <Button variant="ghost">
+            <Text className="text-base">Go to Home</Text>
+          </Button>
         </Link>
-      </ThemedView>
-    </>
+      </View>
+    </ScreenView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
