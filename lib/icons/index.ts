4c2c5ba6b74a6c7a2084dfc9321e8eac4523e6c3
Haw Wei <47056525+hawwei913@users.noreply.github.com>
2025-07-13 22:18:06 +0800

    feat: add new icons to the icons index

--- lib/icons/index.ts
+++ lib/icons/index.ts
@@ -2,5 +2,9 @@ export { HistoryIcon } from './History';
 export { HouseIcon } from './House';
 export { SendIcon } from './Send';
 
-export { UsersIcon } from './Users';
+export { ArrowLeftIcon } from './ArrowLeft';
+export { CircleCheckBigIcon } from './CircleCheckBig';
+export { CircleXIcon } from './CircleX';
 export { PlusIcon } from './Plus';
+export { UsersIcon } from './Users';
+
